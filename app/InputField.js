67c8044706c319eb0dd2ed544625c55530675175
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-04 19:30:14 +0530

    Update InputField.js

--- app/InputField.js
+++ app/InputField.js
@@ -1,7 +1,7 @@
 'use client'
 import React ,{useRef,useState,useEffect,forwardRef} from 'react';
 const InputField=forwardRef((props, ref)=>{
-    const ref = useRef()
+    //const ref = useRef()
     return(
     <div>
       <input id="input" type="text"  ref={ref}/>
